0626a9b9fa83d76c07e6786a9c045a5ab55b46ec
davidaqc <davidqc05@gmail.com>
2021-06-16 12:17:08 -0600

    Vista Login-Cliente

--- webApp/src/app/app-routing.module.ts
+++ webApp/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Importar(injectar) componentes para manejar las rutas
+
+import { LoginClienteComponent } from './componentes/cliente/login-cliente/login-cliente.component';
+
+const routes: Routes = [
+  {path: '', redirectTo:'login', pathMatch:'full'}, // ruta por defecto
+  {path:'login', component:LoginClienteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+// Exportar todos los componentes que estan en router
+export const routingComponents = [LoginClienteComponent]
