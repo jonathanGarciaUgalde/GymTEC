131c2974f2c217497088fb096af0eebf5f5e17e7
Cubero2112 <josuecub.mont@hotmail.com>
2021-06-16 01:33:52 -0600

    Add project files.

---
+++ APIGymTEC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace APIGymTEC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
