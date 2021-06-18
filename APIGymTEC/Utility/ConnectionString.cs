acdf7d0e16b72c9a4dbb3f1dcd5bfb42c591c794
Cubero2112 <josuecub.mont@hotmail.com>
2021-06-18 03:35:18 -0600

    The get action does not  support the conversion between bit and boolen values.

--- APIGymTEC/Utility/ConnectionString.cs
+++ APIGymTEC/Utility/ConnectionString.cs
@@ -2,7 +2,7 @@
 {
     public class ConnectionString
     {
-        private static string cName = "Data Source=.; Initial Catalog=GymTEC;User ID=sa;Password=josuecubero070699.18";
+        private static string cName = "Data Source=.; Initial Catalog=GymTEC;User ID=sa;Password=<Password>";
         public static string CName
         {
             get => cName;
