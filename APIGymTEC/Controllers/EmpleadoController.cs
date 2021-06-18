d635ac6c07d04efc8f3d5bcb7ab473e14d9228a2
Cubero2112 <josuecub.mont@hotmail.com>
2021-06-18 04:07:54 -0600

    Controllers was created.

---
+++ APIGymTEC/Controllers/EmpleadoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APIGymTEC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadoController : ControllerBase
+    {
+        // GET: api/<EmpleadoController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+
+        // GET api/<EmpleadoController>/5
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            return Ok();
+        }
+
+        // POST api/<EmpleadoController>
+        [HttpPost]
+        public ActionResult Post([FromBody] string value)
+        {
+            return Ok();
+        }
+
+        // PUT api/<EmpleadoController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok();
+        }
+
+        // DELETE api/<EmpleadoController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ APIGymTEC/Controllers/PuestoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APIGymTEC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PuestoController : ControllerBase
+    {
+        // GET: api/<PuestoController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+
+        // GET api/<PuestoController>/5
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            return Ok();
+        }
+
+        // POST api/<PuestoController>
+        [HttpPost]
+        public ActionResult Post([FromBody] string value)
+        {
+            return Ok();
+        }
+
+        // PUT api/<PuestoController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok();
+        }
+
+        // DELETE api/<PuestoController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
