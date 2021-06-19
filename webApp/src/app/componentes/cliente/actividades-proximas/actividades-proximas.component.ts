47a7bce3caff3099f82c31a717810ca3928b90e1
davidaqc <davidqc05@gmail.com>
2021-06-19 10:25:59 -0600

    Vista Actividades-Proximas.

---
+++ webApp/src/app/componentes/cliente/actividades-proximas/actividades-proximas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-actividades-proximas',
+  templateUrl: './actividades-proximas.component.html',
+  styleUrls: ['./actividades-proximas.component.css']
+})
+export class ActividadesProximasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
