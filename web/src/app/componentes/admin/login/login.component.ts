2b6c23e3e1e189b87a179c3164f8a0575a84a96c
davidaqc <davidqc05@gmail.com>
2021-06-22 12:34:06 -0600

    Funcionalidad Router en Login-Admin

--- web/src/app/componentes/admin/login/login.component.ts
+++ web/src/app/componentes/admin/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {AdminService} from 'src/app/servicios/admin/admin.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,11 +10,11 @@ import {AdminService} from 'src/app/servicios/admin/admin.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(public api:AdminService) { }
+  constructor(private router:Router, public api:AdminService) { }
 
   public datosUsuario = {
-    Email:null,
-    Password:null
+    Email:"",
+    Password:""
   }
 
   ngOnInit(): void {
@@ -21,22 +22,24 @@ export class LoginComponent implements OnInit {
 
   verificarCredenciales():void {
 
-  	this.api.LoginAdmin(this.datosUsuario.Email, this.datosUsuario.Password)
-  	.subscribe(response=>{
-
-      if (response !== null){
-        console.log(response);
-        
-        /*var cedulaCliente = response.toString();
-        this.router.navigate(["/actividades-proximas", { cedula: cedulaCliente }]);*/
+      if(this.datosUsuario.Email != "" && this.datosUsuario.Password != ""){
+        this.api.LoginAdmin(this.datosUsuario.Email, this.datosUsuario.Password)
+        .subscribe(response=>{
+    
+          if (response !== null){
+            alert("Bienvenido al sistema Administrativo. Su puesto es como: " + response.toString());
+            this.router.navigate(["/admin/sucursales"]);
+          }else{
+            alert("Usuario o contraseña incorrecta, intente de nuevo");
+          }
+          
+        },(error:any)=>{
+            alert("¡Error al intentar conectar con el server!");
+            this.datosUsuario.Password = "";
+          });
       }else{
-        alert("Usuario o contraseña incorrecta, intente de nuevo");
+        alert("Debe ingresar su correo y contraseña");
       }
-      
-  	},(error:any)=>{
-        alert("¡Error al intentar conectar con el server!");
-        this.datosUsuario.Password = "";
-      });
   }
 
 }
