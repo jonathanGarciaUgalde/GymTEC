f3553b2062ed0587fa53db8c753cd45624ca0fc1
davidaqc <davidqc05@gmail.com>
2021-06-22 09:53:49 -0600

    Interfaces Cliente

---
+++ web/src/app/interfaces/usuario.interface.ts
@@ -0,0 +1,14 @@
+export interface Usuario {
+
+	nombre?: string,
+	apellidos?: string,
+    cedula?: number,
+    edad?: number,
+    fecha_nacimiento?: string,
+    peso?: number,
+    imc?: number,
+    direccion?: string,
+    correo?: string,
+	contrasena?: string
+
+}
